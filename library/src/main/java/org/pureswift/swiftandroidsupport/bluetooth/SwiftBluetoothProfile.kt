53667d42e587e9886bd2af88dec1d71b2b8513d4
jmarkstar <mestrella.1991@gmail.com>
2018-06-07 12:15:00 -0500

    Added `SwiftBluetoothProfile`

---
+++ library/src/main/java/org/pureswift/swiftandroidsupport/bluetooth/SwiftBluetoothProfile.kt
@@ -0,0 +1,30 @@
+package org.pureswift.swiftandroidsupport.bluetooth
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothProfile
+
+@SuppressWarnings("JniMissingFunction")
+@SuppressLint("MissingPermission")
+class SwiftBluetoothProfile(private val __swiftObject: Long): BluetoothProfile {
+
+    override fun getDevicesMatchingConnectionStates(states: IntArray?): MutableList<BluetoothDevice> {
+        return __get_devices_matching_connection_states(__swiftObject, states)
+    }
+
+    override fun getConnectionState(device: BluetoothDevice?): Int {
+        return __get_connection_state(__swiftObject, device)
+    }
+
+    override fun getConnectedDevices(): MutableList<BluetoothDevice> {
+        return __get_connected_devices(__swiftObject)
+    }
+
+    //Native Methods
+
+    external fun __get_devices_matching_connection_states(swiftObject: Long, states: IntArray?): MutableList<BluetoothDevice>
+
+    external fun __get_connection_state(swiftObject: Long, device: BluetoothDevice?): Int
+
+    external  fun __get_connected_devices(swiftObject: Long): MutableList<BluetoothDevice>
+}
