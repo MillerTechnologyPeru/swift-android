3afe2bb8784b35bca39fe7168738a865ed7cbc2a
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-06-22 12:51:28 -0500

    Working on MainActivity

--- app/src/main/swift/Sources/MainActivity.swift
+++ app/src/main/swift/Sources/MainActivity.swift
@@ -12,6 +12,37 @@ import java_lang
 import java_util
 import Android
 
+/// Needs to be implemented by app.
+@_silgen_name("SwiftAndroidMainActivity")
+public func SwiftAndroidMainActivity() -> SwiftSupportAppCompatActivity {
+    
+    return MainActivity()
+}
+
+// Like AppDelegate in iOS
+final class MainActivity: SwiftSupportAppCompatActivity {
+    
+    func onCreate(savedInstanceState: Android.OS.Bundle?) {
+        
+        NSLog("Swift \(#function)")
+    }
+    
+    func onResume() {
+        
+        NSLog("Swift \(#function)")
+    }
+    
+    func onPause() {
+        
+        NSLog("Swift \(#function)")
+    }
+    
+    func onRequestPermissionsResult(requestCode: Int, permissions: [String], grantResults: [Int]) {
+        
+        NSLog("Swift \(#function)")
+    }
+}
+
 final class SwiftBluetoothScannerBinding_ListenerImpl: SwiftBluetoothScannerBinding_ListenerBase {
     
     // one-off call to bind the Java and Swift sections of app
