e3350435dabb8aba7967708a62c3bb01388c1c0a
jmarkstar <mestrella.1991@gmail.com>
2018-07-13 16:14:33 -0500

    Updated Android version.

--- swiftdemo/src/main/swift/Package.swift
+++ swiftdemo/src/main/swift/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "swiftdemo", type: .dynamic, targets: ["swiftdemotarget"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:PureSwift/Android.git", .revision("a155c64492d68bc2f29406d65e72bfed6f22ba2c"))
+        .package(url: "git@github.com:PureSwift/Android.git", .revision("9004f9a255513976bc763eb75d48bb6d9057d049"))
     ],
     targets: [
         .target(
