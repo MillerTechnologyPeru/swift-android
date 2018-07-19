383ff14d8031703b151b8c2104b2be64e418253c
Carlos Duclos <darkzeratul64@gmail.com>
2018-07-19 18:52:49 -0500

    added ios demo

---
+++ androiduikitdemo/src/main/swift/iOSDemo/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  iOSDemo
+//
+//  Created by Carlos Duclos on 7/19/18.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        let uiView1 = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        uiView1.backgroundColor = .purple
+        view.addSubview(uiView1)
+        
+        let uiView2 = UIView(frame: CGRect(x: 200, y: 100, width: 100, height: 100))
+        uiView2.backgroundColor = uiView1.backgroundColor
+        view.addSubview(uiView2)
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapped(gesture:)))
+        view.addGestureRecognizer(gesture)
+        
+//        view.isUserInteractionEnabled = false
+        uiView1.isOpaque = false
+        uiView1.center = view.center
+        uiView2.frame = CGRect(x: 0, y: view.frame.height - uiView1.frame.height, width: uiView1.frame.width, height: uiView1.frame.height)
+    }
+    
+    @objc func tapped(gesture: UITapGestureRecognizer) {
+        print("tapped")
+    }
+    
+}
