d50809359f0298dad990be2cc4082cd3409a4834
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-07-20 22:47:47 -0500

    Working on UIView

--- androiduikitdemo/src/main/swift/Sources/ViewController.swift
+++ androiduikitdemo/src/main/swift/Sources/ViewController.swift
@@ -18,6 +18,12 @@ final class MainViewController: UIViewController {
     
     private(set) weak var view1: UIView!
     
+    private(set) weak var view2: UIView!
+    
+    private(set) weak var view3: UIView!
+    
+    private(set) weak var view4: UIView!
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -50,9 +56,27 @@ final class MainViewController: UIViewController {
         view.addSubview(view1)
         self.view1 = view1
         
-        //let view2 = UIView(frame: CGRect(x: 10, y: 10, width: 580, height: 580))
-        //view2.backgroundColor = .green
-        //view1.addSubview(view2)
+        let view2 = UIView(frame: CGRect(x: 10, y: 10,
+                                         width: view1.bounds.size.width - 20,
+                                         height: view1.bounds.size.height - 20))
+        view2.backgroundColor = .green
+        view1.addSubview(view2)
+        self.view2 = view2
+        
+        let view3 = UIView(frame: CGRect(x: 50, y: 50,
+                                         width: view2.bounds.size.width - 100,
+                                         height: view2.bounds.size.height - 100))
+        view3.backgroundColor = .yellow
+        view2.addSubview(view3)
+        self.view3 = view3
+        
+        let view4 = UIView(frame: CGRect(x: view1.frame.origin.x + 40,
+                                         y: view1.frame.origin.y + view1.frame.size.height + 20,
+                                         width: view1.frame.size.width - 80,
+                                         height: 100))
+        view4.backgroundColor = .white
+        view.addSubview(view4)
+        self.view4 = view4
         
         printViews()
     }
@@ -79,6 +103,9 @@ final class MainViewController: UIViewController {
         NSLog("\(#function)")
         NSLog("View: \(view.frame)")
         NSLog("View1: \(view1.frame)")
+        NSLog("View2: \(view2.frame)")
+        NSLog("View3: \(view3.frame)")
+        NSLog("View4: \(view4.frame)")
     }
     
     /*
