9734cdc93a8756d87746457032365fc95f6faea1
jmarkstar <mestrella.1991@gmail.com>
2018-10-30 22:31:26 -0500

    Working on UITabBarController and UINavigationViewController.

--- androiduikitdemo/src/main/swift/Sources/TestUITabBarViewController.swift
+++ androiduikitdemo/src/main/swift/Sources/TestUITabBarViewController.swift
@@ -40,7 +40,7 @@ final class TestUITabBarViewController: UITabBarController {
         third.navigationItem = thirdVCNavItem
         third.tabBarItem = UITabBarItem.init(title: "Chat", image: UIImage.init(named: "ic_chat"), tag: 2)
         
-        let fourth = UINavigationController(rootViewController: UITableTestViewController())
+        let fourth = UINavigationController(rootViewController: NavFirstViewController())
         fourth.tabBarItem = UITabBarItem.init(title: "Fourth", image: UIImage.init(named: "ic_android"), tag: 3)
         
         viewControllers = [first, second, third, fourth]
@@ -96,5 +96,154 @@ final class FirstViewController: UIViewController {
         NSLog("\(type(of: self)) \(#function)")
         
         self.view.backgroundColor = UIColor.orange
+    }
+}
+
+final class NavFirstViewController: UIViewController {
+    
+    override func loadView() {
+        self.view = UIView(frame: CGRect(x: 0,
+                                         y: 0,
+                                         width: UIApplication.shared.androidActivity.screen.bounds.width,
+                                         height: UIApplication.shared.androidActivity.screen.bounds.height))
+    }
+    
+    override func viewDidLoad() {
+        
+        NSLog("\(type(of: self)) \(#function)")
+        
+        self.view.backgroundColor = UIColor.red
+        
+        navigationItem.title = "First Nav VC"
+        
+        let rightItem = UIBarButtonItem.init(title: "Second", style: .done, target: nil, action: nil)
+        rightItem.action = {
+            
+            let secondViewController = NavSecondViewController()
+            self.navigationController?.pushViewController(secondViewController, animated: false)
+        }
+        
+        navigationItem.rightBarButtonItems = [rightItem]
+        
+        //TOOLBAR
+        
+        var items = [UIBarButtonItem]()
+        
+        let toolbarItem1 = UIBarButtonItem(title: "Breakfast", style: .done, target: self) {
+            
+            NSLog("Toolbar Item 1 clicked")
+        }
+        toolbarItem1.image = UIImage(named: "ic_free_breakfast")
+        
+        let toolbarItem2 = UIBarButtonItem(title: "Restaurant", style: .done, target: self) {
+            
+            NSLog("Toolbar Item 2 clicked")
+        }
+        toolbarItem2.image = UIImage.init(named: "ic_restaurant_menu")
+        
+        let toolbarItem3 = UIBarButtonItem(title: "Chat", style: .done, target: self) {
+            
+            NSLog("Toolbar Item 3 clicked")
+        }
+        
+        toolbarItem3.image = UIImage(named: "ic_chat")
+        
+        items.append(toolbarItem1)
+        items.append(toolbarItem2)
+        items.append(toolbarItem3)
+        
+        self.navigationController?.isToolbarHidden = false
+        self.navigationController?.toolbar.items = items
+    }
+}
+
+final class NavSecondViewController: UIViewController {
+    
+    override func loadView() {
+        self.view = UIView(frame: CGRect(x: 0,
+                                         y: 0,
+                                         width: UIApplication.shared.androidActivity.screen.bounds.width,
+                                         height: UIApplication.shared.androidActivity.screen.bounds.height))
+    }
+    
+    override func viewDidLoad() {
+        
+        NSLog("\(type(of: self)) \(#function)")
+        
+        self.view.backgroundColor = UIColor.purple
+        
+        navigationItem.title = "Second Nav VC"
+        
+        let rightItem = UIBarButtonItem.init(title: "Click", style: .done, target: nil, action: nil)
+        rightItem.action = {
+            
+            let thirdViewController = NavThirdViewController()
+            self.navigationController?.pushViewController(thirdViewController, animated: false)
+        }
+        
+        navigationItem.rightBarButtonItems = [rightItem]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isToolbarHidden = true
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+}
+
+final class NavThirdViewController: UIViewController {
+    
+    override func loadView() {
+        self.view = UIView(frame: CGRect(x: 0,
+                                         y: 0,
+                                         width: UIApplication.shared.androidActivity.screen.bounds.width,
+                                         height: UIApplication.shared.androidActivity.screen.bounds.height))
+    }
+    
+    override func viewDidLoad() {
+        
+        NSLog("\(type(of: self)) \(#function)")
+        
+        self.view.backgroundColor = UIColor.yellow
+        
+        navigationItem.title = "Third Nav VC"
+        
+        let rightItem = UIBarButtonItem.init(title: "Second", style: .done, target: nil, action: nil)
+        rightItem.action = {
+            
+            NSLog("HI5")
+        }
+        
+        navigationItem.rightBarButtonItems = [rightItem]
+        
+        //TOOLBAR
+        
+        var items = [UIBarButtonItem]()
+        
+        let toolbarItem1 = UIBarButtonItem(title: "Breakfast", style: .done, target: self) {
+            
+            NSLog("Toolbar Item 1 clicked")
+        }
+        toolbarItem1.image = UIImage(named: "ic_free_breakfast")
+        
+        let toolbarItem2 = UIBarButtonItem(title: "Restaurant", style: .done, target: self) {
+            
+            NSLog("Toolbar Item 2 clicked")
+        }
+        toolbarItem2.image = UIImage.init(named: "ic_restaurant_menu")
+        
+        let toolbarItem3 = UIBarButtonItem(title: "Chat", style: .done, target: self) {
+            
+            NSLog("Toolbar Item 3 clicked")
+        }
+        
+        toolbarItem3.image = UIImage(named: "ic_chat")
+        
+        items.append(toolbarItem1)
+        items.append(toolbarItem2)
+        items.append(toolbarItem3)
+        
+        self.navigationController?.isToolbarHidden = false
+        self.navigationController?.toolbar.items = items
     }
 }
