e2b3febf3b1fe22961f0f7c52b5ec221776c707c
jmarkstar <mestrella.1991@gmail.com>
2019-01-08 12:51:02 -0500

    Tested `UISearchBar` with `UITableView`

--- androiduikitdemo/src/main/swift/Sources/UISearchBarViewControler.swift
+++ androiduikitdemo/src/main/swift/Sources/UISearchBarViewControler.swift
@@ -16,6 +16,12 @@ import AndroidUIKit
 
 final class UISearchBarViewControler: UIViewController {
     
+    private var tableView: UITableView?
+    
+    var searchActive : Bool = false
+    var data = ["San Francisco","New York","San Jose","Chicago","Los Angeles","Austin","Seattle"]
+    var filtered:[String] = []
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -29,14 +35,66 @@ final class UISearchBarViewControler: UIViewController {
     override func viewDidLoad() {
         NSLog("viewDidLoad")
         
+        self.view.backgroundColor = .white
+        
         navigationItem.title = "Test SearchBar"
         
-        let searchBar = UISearchBar(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        let searchBar = UISearchBar(frame: CGRect(x: 20, y: 10, width: 250, height: 50))
         searchBar.placeholder = "Search..."
         searchBar.delegate = self
+        #if os(Android) || os(macOS)
+        searchBar.androidSearchViewExpand()
+        #endif
         
         self.view.addSubview(searchBar)
+        
+        tableView = UITableView(frame: CGRect(x: 10, y: 75, width: 200, height: 300))
+        
+        guard let tableView = tableView else {
+            fatalError("Missing table view")
+        }
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellReuseIdentifier")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        self.view.addSubview(tableView)
     }
+}
+
+extension UISearchBarViewControler: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier", for: indexPath)
+        
+        if(searchActive){
+            cell.textLabel?.text = filtered[indexPath.row]
+        } else {
+            cell.textLabel?.text = data[indexPath.row];
+        }
+        
+        return cell;
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if(searchActive) {
+            return filtered.count
+        }
+        return data.count;
+    }
+    
+}
+
+extension UISearchBarViewControler: UITableViewDelegate {
+    
+    
 }
 
 extension UISearchBarViewControler: UISearchBarDelegate {
@@ -44,19 +102,38 @@ extension UISearchBarViewControler: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange: String) {
         
         log("textDidChange = \(textDidChange)")
+        
+        filtered = data.filter { $0.lowercased().contains(textDidChange) }
+        
+        if(filtered.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+        self.tableView?.reloadData()
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         
         log("\(#function)")
+        searchActive = false;
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-        let query = searchBar.text
+        log("\(#function)")
+        searchActive = false;
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         
-        log("\(#function): query: \(query)")
+        log("\(#function)")
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
         
-        searchBar.text = ""
+        log("\(#function)")
+        searchActive = false;
     }
 }
