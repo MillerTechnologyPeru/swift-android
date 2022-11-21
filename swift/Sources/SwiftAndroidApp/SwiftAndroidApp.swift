4555f8216b07f38fa248ca50afda4e7e0d7ca5b3
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-11-21 15:19:31 -0800

    Updated `didLaunch()`

--- swift/Sources/SwiftAndroidApp/SwiftAndroidApp.swift
+++ swift/Sources/SwiftAndroidApp/SwiftAndroidApp.swift
@@ -2,10 +2,20 @@ import Foundation
 import Android
 
 public final class SwiftAndroidApp: SwiftApplication {
+
+    static let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .full
+        formatter.dateStyle = .full
+        return formatter
+    }()
     
     @_silgen_name("Java_com_pureswift_swiftandroid_Application_00024Companion_didLaunch")
     public static func didLaunch() {
         NSLog("Launching Android app")
+        let date = Date()
+        NSLog("\(formatter.string(from: date))")
+        NSLog("\(UUID())")
     }
 
     @_silgen_name("SwiftAndroidMainActivity")
@@ -18,5 +28,5 @@ public final class SwiftAndroidApp: SwiftApplication {
     public func SwiftAndroidMainApplication() -> SwiftApplication.Type {
         NSLog("\(#function)")
         return SwiftAndroidApp.self
-    }   
+    }
 }
