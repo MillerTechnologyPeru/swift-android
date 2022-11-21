f626b94b7580cb3e6b4a12a9ac210b423e87b84c
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-11-21 11:25:22 -0800

    Add Swift build scripts

--- build-swift.sh
+++ build-swift.sh
@@ -1,15 +1,9 @@
-XCTOOLCHAIN=/Library/Developer/Toolchains/swift-5.7.1-RELEASE.xctoolchain
-SWIFT_SDK=swift-5.7.1-android-aarch64-24-sdk
-
-# Create symbolic link
-rm -rf ./swift/toolchain/$SWIFT_SDK/usr/lib/swift/clang
-ln -sf /usr/lib/clang/13.0.0 \
-    ./swift/toolchain/$SWIFT_SDK/usr/lib/swift/clang
-
-# Generate SwiftPM destination file
-
+#!/bin/bash
+set -e
+source swift-define
 
 # Build with SwiftPM
-$XCTOOLCHAIN/usr/bin/swift build \
-    --destination ./swift/toolchain/android-aarch64.json \
-    --package-path ./swift
+export PATH="$(brew --prefix)/opt/llvm/bin:$PATH"
+$SWIFT_NATIVE_PATH/swift build \
+    --destination $SWIFTPM_DESTINATION_FILE \
+    --package-path $SWIFT_PACKAGE_SRC
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+source swift-define
+
+# Generate SwiftPM destination file
+$SRC_ROOT/generate-swiftpm-toolchain.sh
+
+# Create symbolic link to clang
+rm -rf $SRC_ROOT/swift/toolchain/$SWIFT_SDK/usr/lib/swift/clang
+ln -sf $XCTOOLCHAIN/usr/lib/clang/13.0.0 \
+    $SRC_ROOT/swift/toolchain/$SWIFT_SDK/usr/lib/swift/clang
+
+# Install macOS dependencies
+if [[ $OSTYPE == 'darwin'* ]]; then
+    echo "Install macOS build dependencies"
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install wget cmake ninja android-ndk llvm
+
+    # Verify toolchain installed
+    if [ ! -d ${XCTOOLCHAIN} ]
+    then
+        echo "Please install the swift-5.7.1-RELEASE toolchain (or set XCTOOLCHAIN)"
+        echo "On Mac: https://download.swift.org/swift-5.7.1-release/xcode/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-osx.pkg"
+        exit 1
+    fi
+fi
+
+# Check swift-autolink-extract exists
+if [[ ! -f "${XCTOOLCHAIN}/usr/bin/swift-autolink-extract" ]];
+then
+    echo "Missing symlink '${XCTOOLCHAIN}/usr/bin/swift-autolink-extract'."
+    echo "We need 'sudo' permission to create it (just this once)."
+    sudo ln -s swift ${XCTOOLCHAIN}/usr/bin/swift-autolink-extract || exit 1
+fi
